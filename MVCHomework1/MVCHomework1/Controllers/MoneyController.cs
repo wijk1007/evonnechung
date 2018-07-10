077917b8c27229c07308a78763357372995acbae
wijk1007 <wijk1007@gmail.com>
2018-07-10 14:29:44 +0800

    我的記帳本實作

---
+++ MVCHomework1/MVCHomework1/Controllers/MoneyController.cs
@@ -0,0 +1,37 @@
+﻿using MVCHomework1.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCHomework1.Controllers
+{
+    public class MoneyController : Controller
+    {
+        // GET: Money
+        public ActionResult Money()
+        {
+            return View();
+        }
+        public ActionResult _MoneyList()
+        {
+            var model = new List<MoneyViewModel>();
+            var classtext = "";
+            for (int i = 0; i < 50; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    classtext = "收入";
+                }
+                else
+                {
+                    classtext = "支出";
+                }
+                model.Add(new MoneyViewModel { No = i + 1, Class = classtext, Date = new System.DateTime(2018, 6, 1).AddDays(+i), Money = ((i + 1) * 100).ToString("#,#", CultureInfo.InvariantCulture) });
+            }
+            return View(model);
+        }
+    }
+}
---
+++ MVCHomework1/MVCHomework1/Models/ViewModels/MoneyViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCHomework1.Models.ViewModels
+{
+    public class MoneyViewModel
+    {
+        public int No { get; set; }
+        public string Class { get; set; }
+        public DateTime Date { get; set; }
+        public string Money { get; set; }
+    }
+}
